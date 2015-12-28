9810946e0fec4986c3d94fe09f1edad5ae07e66f
Justin Peck <jmpeck@up.com>
2015-12-28 13:32:51 -0600

    search HTML files, add ignore folder array

--- rename.js
+++ rename.js
@@ -3,20 +3,20 @@ var fs = require('fs'),
     nameFrom = process.argv[2],
     nameTo = process.argv[3];
 
+var ignoreFolders = [];
+
 function walk(dir) {
-    var splitFolders = dir.split('\\'),
-        folder = splitFolders[splitFolders.length - 1];
-    if (folder !== 'assets' && folder !== '.git') {
-        fs.readdirSync(dir).forEach(name => {
-            var fPath = path.join(dir, name);
-            var stat = fs.statSync(fPath);
-            if (stat.isFile() && path.extname(fPath) === '.js') {
-                rename(fPath);
-            } else if (stat.isDirectory()) {
-                walk(fPath);
-            }
-        });
-    }
+    fs.readdirSync(dir).forEach(name => {
+        var fPath = path.join(dir, name);
+        var stat = fs.statSync(fPath),
+            splitFolders = dir.split('\\'),
+            folder = splitFolders[splitFolders.length - 1];
+        if (stat.isFile() && (path.extname(fPath) === '.js' || path.extname(fPath) === '.html')) {
+            rename(fPath);
+        } else if (stat.isDirectory() && ignoreFolders.indexOf(folder) === -1) {
+            walk(fPath);
+        }
+    });
 }
 
 function rename(fPath) {
@@ -24,7 +24,7 @@ function rename(fPath) {
         var reg = new RegExp(nameFrom, 'g'),
             updatedFile = data.replace(reg, nameTo);
         fs.writeFile(fPath, updatedFile, function() {
-            console.log('\x1b[32m', 'UPDATED -- ' + fPath,'\x1b[0m');
+            console.log('\x1b[32m', 'UPDATED -- ' + fPath, '\x1b[0m');
         });
     });
 }
